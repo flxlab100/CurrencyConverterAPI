feedc86d5bca631efafcb10dfe1db446d4b6b714
flxlab100 <flxqlab@gmail.com>
2024-05-16 21:58:29 +0930

    added to py file

--- currency.py
+++ currency.py
@@ -4,14 +4,26 @@ BASE_URL=f'https://api.freecurrencyapi.com/v1/latest?apikey={API_KEY}'
 CURRENCIES= ['USD','CAD','EUR','AUD','CNY']
 
 def convert_currency(base):
-    currencies=','.join(CURRENCIES)
-    url=f'{BASE_URL}&base_currency={base}&currencies={currencies}'
+    currencies = ",".join(CURRENCIES)
+    url = f"{BASE_URL}&base_currency={base}&currencies={currencies}"
     try:
-       reponese=requests.get(url)
-       data=reponese.json()
-       print(data)
-       return data
-    except Exception as e:
-        print(e)
+        response = requests.get(url)
+        data = response.json()
+        return data["data"]
+    except:
+        print("Invalid currency.")
         return None
-convert_currency('CAD')
+
+while True:
+    base = input("Enter the base currency (q for quit): ").upper()
+
+    if base == "Q":
+        break
+
+    data = convert_currency(base)
+    if not data:
+        continue
+
+    del data[base]
+    for ticker, value in data.items():
+        print(f"{ticker}: {value}")
